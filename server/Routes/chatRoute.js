b44eb412e6d8959ba0088a221d46ceddae7b96d8
aishwary915 <aishwarymalviya555@gmail.com>
2025-09-15 00:57:26 +0530

    Added JWT auth middleware and protected chat/message routes

--- server/Routes/chatRoute.js
+++ server/Routes/chatRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { createChat, findUserChat, findChat } = require("../Controller/chatController");
+const verifyToken = require("../Middleware/authMiddleware");
 
 const router = express.Router();
 
-router.post("/",createChat);
-router.get("/:userId",findUserChat);
-router.get("/find/:firstId/:secondId",findChat );
+router.post("/", verifyToken, createChat);
+router.get("/:userId", verifyToken, findUserChat);
+router.get("/find/:firstId/:secondId", verifyToken, findChat );
 
 module.exports = router;
--- server/Routes/messageRoute.js
+++ server/Routes/messageRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { createMessage, getMessages } = require("../Controller/messageController");
+const verifyToken = require("../Middleware/authMiddleware");
 
 const router = express.Router();
 
-router.post("/",createMessage);
-router.get("/:chatId",getMessages);
+router.post("/", verifyToken, createMessage);
+router.get("/:chatId", verifyToken, getMessages);
 
 module.exports = router;
