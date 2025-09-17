c37e48ab8cbdde2b673080dd4082b28144ae599d
aishwary915 <aishwarymalviya555@gmail.com>
2025-09-17 14:50:42 +0530

    feat: add email validation using validator.js in User schema

--- server/Models/userModel.js
+++ server/Models/userModel.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     name:{type: String, required: true, minlength : 3, maxlength: 30},
-    email:{type: String, required: true,minlength: 3,maxlength: 200, unique: true,},
+    email:{type: String, required: true,minlength: 3,maxlength: 200, unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: "Please enter a valid email"
+        }
+    },
     password: {type: String,required: true,minlength: 3,maxlength: 1024},
 },
 {
